b162cc6b0bf1759aeb87e115e2bf31838f6f8370
Cas Fahrenfort <cas.fahrenfort@dimaros.nl>
2021-12-02 22:49:01 +0100

    Simplify Day 2

--- 2/2.hs
+++ 2/2.hs
@@ -5,8 +5,7 @@ import Data.List.Split
 
 data Command = 
     Forward Int |
-    Down Int |
-    Up Int 
+    Depth Int  
 
 main = do  
         contents <- readFile "2.txt"
@@ -15,13 +14,12 @@ main = do
 f :: Int -> Int -> Int -> [Command] -> Int
 f h d a [] = h * d
 f h d a ((Forward x):xs) = f (h+x) (d+a*x) a xs
-f h d a ((Down x):xs) = f h d (a+x) xs
-f h d a ((Up x):xs) = f h d (a-x) xs
+f h d a ((Depth x):xs) = f h d (a+x) xs
 
 readCommand :: String -> Command
 readCommand s@('f':xs) = Forward (readNr s)
-readCommand s@('d':xs) = Down (readNr s)
-readCommand s@('u':xs) = Up (readNr s)
+readCommand s@('d':xs) = Depth (readNr s)
+readCommand s@('u':xs) = Depth (-(readNr s))
 
 readNr :: String -> Int
-readNr s = read $ splitOn " " s !! 1 :: Int
+readNr s = read $ last $ splitOn " " s :: Int
